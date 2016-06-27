b752eceb95b0fd96c59d5c11bec04d44f97a7a75
xubin <x_shares@outlook.com>
2016-06-27 14:53:40 +0800

    command processing framework

--- docmd.c
+++ docmd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <setjmp.h>
 
 #define TOK_ADD		5
 #define NAXLIN		128
@@ -9,9 +10,13 @@ void do_line(char *);
 void cmd_add(void);
 int get_token(void);
 
+jmp_buf jmpbuffer;
+
 int main(void)
 {
 	char 	line[MAXLIN];
+	if(setjmp(jmpbuffer) != 0)
+		printf("error");
 	while((fgets(line,MAXLIN,stdin)) != NULL)
 		do_line(line);
 	exit(0);
@@ -36,8 +41,9 @@ void do_line(char *ptr)		//process one line of input;
 void cmd_add(void)
 {
 	int token;
-	
 	token = get_token();
+	if(token < 0)			/*an error has occurred */
+		longjmp(jmpbuffer,1);
 	//rest of processing for this command
 }
 
